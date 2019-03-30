3277d6aee6dd2dd50e8c2630193e90f82e817df9
Matt <boblikesbbq@gmail.com>
2019-03-30 14:42:34 -0700

    Add dependencies and use body-parser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 
 let port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
   res.send("This will be the home route");
