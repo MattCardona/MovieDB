f1723807f2a44724bd2172c9e40b6fa3721c2139
Matt <boblikesbbq@gmail.com>
2019-04-01 12:31:51 -0700

    Add comments to home route

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req, res) => {
+// will show movies now playing
+app.get("/homepage", (req, res) => {
   res.send("This will be the home route");
 });
 
