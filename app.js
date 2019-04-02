9d1b194121b31c2dea23e51dec581b063d5ed905
Matt <boblikesbbq@gmail.com>
2019-04-02 16:52:32 -0700

    add env variables

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
 
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || process.env.DEV_PORT;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
