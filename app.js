bc6afa5405b37fb72e83bc01cc64714e3d156359
Matt <boblikesbbq@gmail.com>
2019-04-03 15:27:53 -0700

    Finish /homepage route

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 
 let port = process.env.PORT || process.env.DEV_PORT;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get("/", (req, res) => res.redirect("/homepage"))
+
 // will show movies now playing
 app.get("/homepage", (req, res) => {
-  res.send("This will be the home route");
+  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.MOVIE_DB}&language=en-US&page=1`)
+  .then(res => {
+    return res.data;
+  })
+  .then(data => {
+    res.json(data);
+  })
+  .catch(e => {
+    console.log(JSON.stringify(e, undefined, 2));
+    res.send('Oops something went wrong');
+  })
+  
 });
 
 // will search for a specific movie/show title
