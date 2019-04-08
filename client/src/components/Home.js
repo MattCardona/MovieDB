fb0882ffc96a7a9caa79de910321de6a3a7b3564
Matt <boblikesbbq@gmail.com>
2019-04-08 16:27:28 -0700

    Add intial Home page component

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This will be the home Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
