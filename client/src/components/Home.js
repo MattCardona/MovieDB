c0780ee95e0748d043275466ab82bd718afd4f50
Matt <boblikesbbq@gmail.com>
2019-04-10 15:09:41 -0700

    Add MovieInfo component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MovieInfo from './MovieInfo';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>This will be the home Component</h1>
+        <MovieInfo />
       </div>
     )
   }
-}
+};
 
 export default Home;
---
+++ client/src/components/MovieInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieInfo extends Component {
+  render() {
+    return (
+      <div>
+        This will be the MovieInfo Component!
+      </div>
+    )
+  }
+};
+
+export default MovieInfo;
