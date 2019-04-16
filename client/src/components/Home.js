9d9d515267676f261e03615652e3e71638d40007
Matt <boblikesbbq@gmail.com>
2019-04-16 11:52:58 -0700

    make fetch to backend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,18 @@ import MovieInfo from './MovieInfo';
 import SearchMovie from './SearchMovie';
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount(){
+    fetch("/homepage")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then(data =>  console.log(data))
+    .catch(err => console.log(JSON.stringify(err, undefined, 2)))
+  }
   render() {
     return (
       <div>
