4fc59c1d11f856a6484298e4951f6739216dd719
Matt <boblikesbbq@gmail.com>
2019-04-18 16:14:35 -0700

    Create Carousel for Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import MovieInfo from './MovieInfo';
-import SearchMovie from './SearchMovie';
+import Slider from 'react-slick';
 
 class Home extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      movies: [],
+      sliderMovies: []
+    }
   }
   componentDidMount(){
     fetch("/homepage")
     .then((res) => {
-      console.log(res);
       return res.json();
     })
-    .then(data =>  console.log(data))
+    .then(data => {
+      // console.log(data);
+      this.setState(() => ({
+        sliderMovies: data.slice(0, 6),
+        movies: [...data]
+      }))
+    })
     .catch(err => console.log(JSON.stringify(err, undefined, 2)))
   }
   render() {
+    const { sliderMovies } = this.state;
+    const settings = {
+      dots: false,
+      infinite: true,
+      autoplay: true,
+      speed: 2500
+    }
     return (
       <div>
-        <h1>This will be the home Component</h1>
-        
+        <Slider {...settings} >
+          { sliderMovies.map(movie => {
+            return (
+              <div key={movie.id}>
+                <div className="container-fluid"
+                  style={{
+                  backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center center",
+                  height: "100vh"
+                }}
+                >
+                </div>
+              </div>
+            )
+          }) }
+        </Slider>
       </div>
     )
   }
