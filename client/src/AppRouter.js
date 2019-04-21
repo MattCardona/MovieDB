0a2fc0412caab27be159fe60835b0ec2d44383de
Matt <boblikesbbq@gmail.com>
2019-04-21 12:06:06 -0700

    Add route to MovieInfo

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Home from './components/Home';
 import SearchMovie from './components/SearchMovie';
+import MovieInfo from './components/MovieInfo';
 
 const AppRouter = (props) => {
     return (
@@ -11,6 +12,7 @@ const AppRouter = (props) => {
         <Switch>
           <Route path="/" component={ Home } exact />
           <Route path="/search/:movie" component={ SearchMovie } />
+          <Route path="/movie/:id" component={ MovieInfo } />
         </Switch>
       </BrowserRouter>
     );
