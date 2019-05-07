085fb3c9d55de7ac61bd2ff266c88381b8dea656
Matt <boblikesbbq@gmail.com>
2019-05-07 15:43:00 -0700

    Fix Link

--- client/src/components/SearchMovie.js
+++ client/src/components/SearchMovie.js
@@ -52,7 +52,12 @@ class SearchMovie extends Component {
               const movieBackdrop = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
               return (
                 <div className="col-xs-12 col-sm-6 col-md-4" key={movie.id} >
-                  <Link to={`/movie/${movie.id}`}>
+                  <Link 
+                    to={{
+                      pathname: `/movie/${movie.id}`,
+                      state: { prev: 'search' }
+                    }}
+                  >
                     <div className="card">
                       <img className="card-image" src={movieBackdrop} alt=""/>
                       <h4 className="card-title">
