37948bb25722eaf3ad62133731b952d05c031696
Matt <boblikesbbq@gmail.com>
2019-05-07 15:28:07 -0700

    Fix redirect

--- client/src/components/MovieInfo.js
+++ client/src/components/MovieInfo.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class MovieInfo extends Component {
   constructor(props){
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       movie: {},
       height: ""
@@ -27,6 +28,9 @@ class MovieInfo extends Component {
     })
     .catch(err => console.log(JSON.stringify(err, undefined, 2)))
   }
+  handleClick(){
+    this.props.history.goBack()
+  }
   render() {
     const { movie } = this.state;
     return (
@@ -41,7 +45,7 @@ class MovieInfo extends Component {
           <div className="container" id="overview">
             <h1 id="movie-title" >{movie.original_title}</h1>  
             <p>{movie.overview}</p>
-            <Link to="/" >Home <i className="fas fa-home"></i></Link>
+            <i onClick={this.handleClick} className="fas fa-home">Home</i>
           </div>
         </div>
       </div>
