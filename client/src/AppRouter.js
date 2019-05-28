0cdd97cdfdf33841847949b0456433d757d9ade4
Matt <boblikesbbq@gmail.com>
2019-05-28 14:53:16 -0700

    Add Actors to router

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -6,18 +6,20 @@ import Home from './components/Home';
 import SearchMovie from './components/SearchMovie';
 import MovieInfo from './components/MovieInfo';
 import NotFound from './components/NotFound';
+import Actors from './components/Actors/Actors'
 
 const AppRouter = (props) => {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={ Home } exact />
-          <Route path="/search/:movie" component={ SearchMovie } />
-          <Route path="/movie/:id" component={ MovieInfo } />
-          <Route component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/search/:movie" component={SearchMovie} />
+        <Route path="/movie/:id" component={MovieInfo} />
+        <Route path="/actors" component={Actors} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
