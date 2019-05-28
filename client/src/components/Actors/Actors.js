abbd47401aeeee28b1af8b93c3e6c8eb33669e3d
Matt <boblikesbbq@gmail.com>
2019-05-28 14:52:49 -0700

    Create popular actor/actress component

---
+++ client/src/components/Actors/Actors.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Actors extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This will be the popular actors/actresses component</h1>
+      </div>
+    )
+  }
+}
+
+export default Actors;
