5cec0cda7edf17af0ed1b80875c20678e3d7faa5
Matt <boblikesbbq@gmail.com>
2019-05-30 15:39:26 -0700

    Successfully get popular actors from backend

--- client/src/components/Actors/Actors.js
+++ client/src/components/Actors/Actors.js
@@ -1,6 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Actors extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actors: []
+    };
+  }
+  componentDidMount() {
+    fetch("/popularactors")
+      .then((res) => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState(() => ({
+          actors: data
+        }))
+      })
+      .catch(err => console.log(JSON.stringify(err, undefined, 2)))
+  }
   render() {
     return (
       <div>
