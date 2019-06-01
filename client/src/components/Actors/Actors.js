6b1c66825af84d28e0b576a3fa789b59ab913138
Matt <boblikesbbq@gmail.com>
2019-06-01 10:05:36 -0700

    Getting popular actors/actress displayed

--- client/src/components/Actors/Actors.js
+++ client/src/components/Actors/Actors.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import ActorCards from './ActorCards';
 
 class Actors extends Component {
   constructor(props) {
@@ -22,9 +22,15 @@ class Actors extends Component {
       .catch(err => console.log(JSON.stringify(err, undefined, 2)))
   }
   render() {
+    const { actors } = this.state;
     return (
       <div>
         <h1>This will be the popular actors/actresses component</h1>
+        <div className="row">
+          {actors.map(actor => {
+            return <ActorCards {...actor} />
+          })}
+        </div>
       </div>
     )
   }
