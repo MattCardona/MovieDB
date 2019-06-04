2f66add2c9d931051f792111ea5cd3538f36dfa1
Matt <boblikesbbq@gmail.com>
2019-06-04 15:37:43 -0700

    Add MoreInfo component for actor/actresses

---
+++ client/src/components/Actors/MoreInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MoreInfo extends Component {
+  render() {
+    return (
+      <div>
+        This will be the more info page for actor/actress
+      </div>
+    )
+  }
+}
+
+export default MoreInfo;
