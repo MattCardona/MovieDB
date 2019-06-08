1b4359217319c730375e5d7946babe84e31bc4c2
Matt <boblikesbbq@gmail.com>
2019-06-07 17:07:17 -0700

    Successfully get actor info from backend

--- client/src/components/Actors/MoreInfo.js
+++ client/src/components/Actors/MoreInfo.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 
 class MoreInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actor: {}
+    }
+  }
+  componentDidMount() {
+    // console.log(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    fetch(`/actor/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then(data => {
+        // console.log(data);
+        this.setState(() => ({
+          actor: data
+        }));
+      })
+      .catch(err => console.log(JSON.stringify(err, undefined, 2)))
+  }
   render() {
     return (
       <div>
