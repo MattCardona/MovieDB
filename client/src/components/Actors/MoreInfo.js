74769e3756a96a7d81aa86685a53f3630bb59884
Matt <boblikesbbq@gmail.com>
2019-06-10 17:30:26 -0700

    Remove inline styles

--- client/src/components/Actors/MoreInfo.js
+++ client/src/components/Actors/MoreInfo.js
@@ -27,7 +27,7 @@ class MoreInfo extends Component {
     const { actor } = this.state;
     const actorImage = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
     return (
-      <div className="container-fluid" style={{ backgroundColor: "black" }}>
+      <div className="container-fluid" id="actor-more-info">
         <div className="row">
           <div className="col-md-6">
             <div className="card">
@@ -38,13 +38,13 @@ class MoreInfo extends Component {
             </div>
           </div>
 
-          <div className="col-md-6" style={{ textAlign: "center" }}>
-            <p style={{ color: "#fff", paddingTop: "40px" }}>
+          <div className="col-md-6" id="actor-bio">
+            <p>
               {actor.biography}
             </p>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
