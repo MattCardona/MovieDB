11a108806a327b81165f8eefc56eca470317350f
Matt <boblikesbbq@gmail.com>
2019-06-30 12:28:28 -0700

    Add Logo to Navbar with styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
       <div>
         <nav className="navbar  navbar-expand-lg navbar-dark">
-          {/* <a class="navbar-brand" href="#"></a> */}
+          <Link className="navbar-brand" to="/" style={{ fontFamily: 'Cinzel serif', color: "#ffe000", fontSize: "30px" }}>MovieDB</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-burger" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -17,7 +17,7 @@ class Navbar extends Component {
                 <Link to="/" className="nav-link hover-effect">Home <i className="fas fa-home"></i></Link>
               </li>
               <li className="nav-item">
-                <Link to="/actors" className="nav-link hover-effect" >Actors/Actresses <i className="fas fa-user hover-effect"></i> </Link>
+                <Link to="/actors" className="nav-link hover-effect" >Actors/Actresses <i className="fas fa-user hover-effect"></i></Link>
               </li>
             </ul>
           </div>
