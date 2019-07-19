05569f7d5645b5ac336c9fd19da9533a761df0c0
Matt <boblikesbbq@gmail.com>
2019-07-19 09:27:09 -0700

    Add search bar for actors/actresses

--- client/src/components/Actors/Actors.js
+++ client/src/components/Actors/Actors.js
@@ -5,8 +5,11 @@ import Navbar from '../Navbar';
 class Actors extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
-      actors: []
+      actors: [],
+      search: ""
     };
   }
   componentDidMount() {
@@ -22,7 +25,17 @@ class Actors extends Component {
       })
       .catch(err => console.log(JSON.stringify(err, undefined, 2)))
   }
-
+  handleChange(e) {
+    let searchTerm = e.target.value;
+    console.log(searchTerm);
+    this.setState(() => ({
+      search: searchTerm
+    }))
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.search);
+  }
   render() {
     const { actors } = this.state;
 
@@ -30,6 +43,17 @@ class Actors extends Component {
       <div className="popular-actors">
         <Navbar />
         <h1>Popular Actors/Actresses</h1>
+
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.search}
+              onChange={this.handleChange}
+            />
+            <button>Submit</button>
+          </form>
+        </div>
 
         <div className="container-fluid">
           <div className="row">
