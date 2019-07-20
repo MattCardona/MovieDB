29e4d00e78472f6e64327ddeeaf96a25c17f1dcc
Matt <boblikesbbq@gmail.com>
2019-07-20 07:00:15 -0700

    Add SearchActor component

---
+++ client/src/components/Actors/SearchActor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchActor extends Component {
+  render() {
+    return (
+      <div>
+        This is the Search Actor comp
+      </div>
+    )
+  }
+}
+
+export default SearchActor;
