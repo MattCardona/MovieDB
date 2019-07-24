f8c3eba05429afcb24546d961adc7243877b7beb
Matt <boblikesbbq@gmail.com>
2019-07-24 16:11:10 -0700

    Getting data

--- client/src/components/Actors/SearchActor.js
+++ client/src/components/Actors/SearchActor.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class SearchActor extends Component {
+  componentDidMount() {
+    console.log(this.props.match.params.name);
+  }
   render() {
     return (
       <div>
