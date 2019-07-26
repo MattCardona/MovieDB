8d590f2f87208bc1727c8ebbd2f9011f91a106c4
Matt <boblikesbbq@gmail.com>
2019-07-26 13:05:37 -0700

    Adding actors/actreses to state

--- client/src/components/Actors/SearchActor.js
+++ client/src/components/Actors/SearchActor.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 class SearchActor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actors: []
+    };
+  }
   componentDidMount() {
     // console.log(this.props.match.params.name);
     const { name: actor } = this.props.match.params;
     axios.post("/search/person", { actor })
       .then(res => {
         console.log(res.data, "res");
+        this.setState(() => ({
+          actors: res.data
+        }))
       })
       .catch(e => {
         console.log(e, "error");
       })
   }
   render() {
+    let { actors } = this.state;
     return (
       <div>
-        This is the Search Actor comp
+        {actors.map(actor => {
+          let actorImage = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
+          return (
+            <div key={actor.id}>
+              <h1>{actor.name}</h1>
+              <img src={actorImage} alt="" />
+            </div>
+          )
+        })}
       </div>
     )
   }
