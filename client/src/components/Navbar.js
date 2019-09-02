d398c86df416fd8fb7176bf8a1cee50249adae32
Matt <boblikesbbq@gmail.com>
2019-09-02 15:25:53 -0700

    Refactor to stateless functional component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar  navbar-expand-lg navbar-dark my-navbar">
-          <Link className="navbar-brand" to="/" >MovieDB</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-burger" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+const Navbar = (props) => {
+  return (
+    <div>
+      <nav className="navbar  navbar-expand-lg navbar-dark my-navbar">
+        <Link className="navbar-brand" to="/" >MovieDB</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-burger" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <div className="collapse navbar-collapse" id="navbar-burger">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link hover-effect">Home <i className="fas fa-home"></i></Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/actors" className="nav-link hover-effect" >Actors/Actresses <i className="fas fa-user"></i></Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/search" className="nav-link hover-effect" >Movie Search <i className="fas fa-film"></i></Link>
-              </li>
-            </ul>
-          </div>
+        <div className="collapse navbar-collapse" id="navbar-burger">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link hover-effect">Home <i className="fas fa-home"></i></Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/actors" className="nav-link hover-effect" >Actors/Actresses <i className="fas fa-user"></i></Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/search" className="nav-link hover-effect" >Movie Search <i className="fas fa-film"></i></Link>
+            </li>
+          </ul>
+        </div>
 
-        </nav>
-      </div>
-    )
-  }
-}
-
+      </nav>
+    </div>
+  )
+};
 export default Navbar;
