11558dbc39c92f24eeec52b2ab0d6973cf9968db
Matt <boblikesbbq@gmail.com>
2019-09-30 16:48:42 -0700

    Add new type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,4 @@ export const NOW_PLAYING = "NOW_PLAYING";
 export const POPULAR = "POPULAR";
 export const TOP_RATED = "TOP_RATED";
 export const SEARCHED_MOVIE = "SEARCHED_MOVIE";
+export const SEARCH_MOVIE = "SEARCH_MOVIE";
