4590c01c9030e78e2a97ecb34445cf1b7eaa480b
Matt <boblikesbbq@gmail.com>
2019-10-05 15:59:41 -0700

    Add new type SEARCH_MOVIE_ERROR

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,4 @@ export const POPULAR = "POPULAR";
 export const TOP_RATED = "TOP_RATED";
 export const SEARCHED_MOVIE = "SEARCHED_MOVIE";
 export const SEARCH_MOVIE = "SEARCH_MOVIE";
+export const SEARCH_MOVIE_ERROR = "SEARCH_MOVIE_ERROR"; 
