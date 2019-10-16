ee8fa8f86bc447f9d6bfd3e71e6e1a638f67059b
Matt <boblikesbbq@gmail.com>
2019-10-16 16:56:05 -0700

    Add POPULAR_ACTORS type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,4 +3,6 @@ export const POPULAR = "POPULAR";
 export const TOP_RATED = "TOP_RATED";
 export const SEARCHED_MOVIE = "SEARCHED_MOVIE";
 export const SEARCH_MOVIE = "SEARCH_MOVIE";
-export const SEARCH_MOVIE_ERROR = "SEARCH_MOVIE_ERROR"; 
+export const SEARCH_MOVIE_ERROR = "SEARCH_MOVIE_ERROR";
+// actor types
+export const POPULAR_ACTORS = "POPULAR_ACTORS";
