dae4a04b4c90586a3dcee15fe5d839cc7920a015
Matt <boblikesbbq@gmail.com>
2019-10-19 15:49:12 -0700

    Handle POPULAR_ACTORS type

--- client/src/reducers/actorsReducer.js
+++ client/src/reducers/actorsReducer.js
@@ -1,3 +1,5 @@
+import { POPULAR_ACTORS } from "../actions/types";
+
 const actorsInitialState = {
   actor: {},
   actors: [],
@@ -6,6 +8,11 @@ const actorsInitialState = {
 
 const actorsReducer = (state = actorsInitialState, action) => {
   switch (action.type) {
+    case POPULAR_ACTORS:
+      return {
+        ...state,
+        actors: [...action.actors]
+      }
     default:
       return state;
   }
