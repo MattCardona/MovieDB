6cfea6b696e5b70fdb0e1de7406ed8e808f11bd1
Matt <boblikesbbq@gmail.com>
2019-11-19 15:10:01 -0800

    create SEARCHED_MOVIE_NAV type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,6 +4,8 @@ export const TOP_RATED = "TOP_RATED";
 export const SEARCHED_MOVIE = "SEARCHED_MOVIE";
 export const SEARCH_MOVIE = "SEARCH_MOVIE";
 export const SEARCH_MOVIE_ERROR = "SEARCH_MOVIE_ERROR";
+export const SEARCHED_MOVIE_NAV = "SEARCHED_MOVIE_NAV";
+
 // actor types
 export const POPULAR_ACTORS = "POPULAR_ACTORS";
 export const SEARCH_ACTOR = "SEARCH_ACTOR";
