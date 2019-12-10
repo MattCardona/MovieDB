49ed6c73ee7712e3146ac24e06b296b9a1ca2d12
Matt <boblikesbbq@gmail.com>
2019-12-09 18:43:10 -0800

    Create router for movies routes

--- app.js
+++ app.js
@@ -5,101 +5,15 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 const path = require("path");
 
+const movies = require("./routes/movies");
+
 let port = process.env.PORT || process.env.DEV_PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/movies", movies);
 // app.get("/", (req, res) => res.redirect("/homepage"))
-
-// will show movies now playing
-app.get("/homepage", (req, res) => {
-  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.MOVIE_DB}&language=en-US&page=1`)
-    .then(res => {
-      return res.data;
-    })
-    .then(data => {
-      let { results } = data;
-      // console.log(data.results);
-      res.status(200).json(results);
-    })
-    .catch(e => {
-      // console.log(JSON.stringify(e, undefined, 2));
-      res.status(404).json({ msg: "There is nothing to show at this moment" });
-    });
-
-});
-
-// will show popular movies
-app.get("/popular", (req, res) => {
-  axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_DB}&language=en-US&page=1`)
-    .then(res => {
-      return res.data;
-    })
-    .then(data => {
-      let { results } = data;
-      // console.log(data.results);
-      res.status(200).json(results);
-    })
-    .catch(e => {
-      // console.log(JSON.stringify(e, undefined, 2));
-      res.status(404).json({ msg: "There is nothing to show at this moment" });
-    });
-
-});
-
-// will show top rated movies
-app.get("/toprated", (req, res) => {
-  axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_DB}&language=en-US&page=1`)
-    .then(res => {
-      return res.data;
-    })
-    .then(data => {
-      let { results } = data;
-      // console.log(data.results);
-      res.status(200).json(results);
-    })
-    .catch(e => {
-      // console.log(JSON.stringify(e, undefined, 2));
-      res.status(404).json({ msg: "There is nothing to show at this moment" });
-    });
-
-});
-
-// will search for a specific movie/show title
-app.post("/movies", (req, res) => {
-  const { movie } = req.body;
-  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_DB}&language=en-US&page=1&include_adult=true&query=${movie}`)
-    .then(res => res.data)
-    .then(data => {
-      let { results } = data;
-      // console.log(data);
-      if (results.length > 0) {
-        return res.status(200).json(results);
-      } else {
-        return res.status(404).json({ error: "No movie/tv show found" });
-      }
-
-    })
-    .catch(e => {
-      // console.log(JSON.stringify(e, undefined, 2));
-      return res.status(404).json({ error: "No movie/tv show found" });
-    });
-});
-
-// will get more info on specific movie/show
-app.get("/movies/:id", (req, res) => {
-  const { id } = req.params;
-
-  axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.MOVIE_DB}&language=en-US&external_source=imdb_id`)
-    .then(res => res.data)
-    .then(data => res.status(200).json(data))
-    .catch(e => {
-      // console.log(e);
-      res.status(404).redirect("/");
-    });
-
-});
 
 // ============================
 // actor / actresses routes
