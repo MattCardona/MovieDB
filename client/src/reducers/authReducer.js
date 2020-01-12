7a2e1311efefa8e26c63263463cdf5be34e666c7
Matt <boblikesbbq@gmail.com>
2020-01-12 14:09:08 -0800

    Initial authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isAuthenticated: null,
+  token: null
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
