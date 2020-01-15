1821e00e5194f30d0b83671ca2eeb6a5d1b426a5
Matt <boblikesbbq@gmail.com>
2020-01-14 20:13:42 -0800

    Create initial Signup component

---
+++ client/src/components/Users/Signup.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Navbar from '../Navbar';
+import Axios from 'axios';
+
+
+export default class Signup extends Component {
+  state = {
+    username: "",
+    password: ""
+  }
+  onChange = (e) => {
+    let prop = e.target.name;
+    let val = e.target.value;
+
+    this.setState(() => ({
+      [prop]: val
+    }))
+  }
+  onSubmit = (e) => {
+    // here call action to signup user to the backend
+    e.preventDefault();
+    console.log(this.state);
+    Axios.post("/signup", this.state)
+      .then(({ data }) => {
+        console.log(data.token);
+      })
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <div
+        className="container-fluid"
+        style={{ backgroundColor: "black", textAlign: "center", color: "#fff" }}
+      >
+        <Navbar />
+        <h1>Sign up</h1>
+        <form onSubmit={this.onSubmit}
+          style={{ textAlign: "center" }}
+        >
+          <div className="form-group">
+            <label htmlFor="username"></label>
+            <input
+              id="username"
+              style={{ textAlign: "center", width: "50%" }}
+              type="text"
+              name="username"
+              onChange={this.onChange}
+              value={this.state.username}
+              placeholder="username"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password"></label>
+            <input
+              style={{ textAlign: "center", width: "50%" }}
+              type="password"
+              id="password"
+              name="password"
+              onChange={this.onChange}
+              value={this.state.password}
+              placeholder="password"
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
