967fe67f6f62e0d6b56ed736dd91a8227deac87b
Matt <boblikesbbq@gmail.com>
2020-01-23 14:02:55 -0800

    save users token to localStorage

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,9 +1,12 @@
 import Axios from "axios";
 import { SIGNUP_USER } from "./types";
 
+// if not return user a error
 export const signup = (user, cb) => async dispatch => {
   try {
     const { data } = await Axios.post("/signup", user);
+    // need to save token to localstorage 
+    localStorage.setItem("token", data.token);
     dispatch({
       type: SIGNUP_USER,
       token: data.token
