11cc0a8210649b8ca41c2da4532d6eddad9a71e8
Matt <boblikesbbq@gmail.com>
2020-01-31 05:52:38 -0800

    Replace bcrypt with bcryptjs package

--- middleware/authroutesMiddleware.js
+++ middleware/authroutesMiddleware.js
@@ -1,5 +1,5 @@
 const Users = require("../models/Users");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const createToken = ({ _id, username }) => {
