01f821ca58aeee1268a63448bb98aa68b3f05dc8
Matt <boblikesbbq@gmail.com>
2020-02-02 13:05:40 -0800

    decode token to get users id

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { SIGNUP_USER } from "./types";
 import setAuthToken from "../utils/setAuthToken";
+import decode from 'jwt-decode'
 
 // if not return user a error
 export const signup = (user, cb) => async dispatch => {
@@ -8,7 +9,11 @@ export const signup = (user, cb) => async dispatch => {
     const { data } = await Axios.post("/signup", user);
     // need to save token to localstorage 
     localStorage.setItem("token", data.token);
+    // create method to set auth header with token on axios calls
     setAuthToken(data.token);
+    // get user from token and set the usersId to the redux state with JWT-decode
+    let { _id } = decode(data.token);
+    console.log(_id);
     dispatch({
       type: SIGNUP_USER,
       token: data.token
