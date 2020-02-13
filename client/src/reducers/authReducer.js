7533ad84698ceab3f1ddec9d406dd72d4819d410
Matt <boblikesbbq@gmail.com>
2020-02-13 13:09:55 -0800

    Set initial state refactor add userId

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,16 +1,26 @@
 import { SIGNUP_USER } from "../actions/types";
+import setAuthToken from "../utils/setAuthToken";
+import decode from 'jwt-decode'
+
+
+let token = localStorage.getItem("token");
+let userId = "";
+if (token) {
+  setAuthToken(token);
+  userId = decode(token)._id;
+}
 
 const initialState = {
-  isAuthenticated: null,
-  token: null
+  isAuthenticated: token,
+  userId
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SIGNUP_USER:
       return {
-        isAuthenticated: true,
-        token: action.token
+        isAuthenticated: action.token,
+        userId: action.userId
       }
     default:
       return state;
