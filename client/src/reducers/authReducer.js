b8f616322d374b7a4f0a52ac87ce8705bd005c99
Matt <boblikesbbq@gmail.com>
2020-03-03 12:11:13 -0800

    handle expired tokens

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,38 @@
 import { SIGNUP_USER, SIGNIN_USER, SIGNOUT_USER } from "../actions/types";
 import setAuthToken from "../utils/setAuthToken";
-import decode from 'jwt-decode'
+import decode from 'jwt-decode';
 
 let token = localStorage.getItem("token");
 let userId = "";
-if (token) {
-  setAuthToken(token);
-  userId = decode(token)._id;
+
+const checkTokenExp = token => {
+  const user = decode(token);
+  const currentTime = Date.now() / 1000;
+
+  if (user.exp < currentTime) {
+    // console.log("token exp");
+    token = null;
+    localStorage.removeItem("token");
+    window.location.href = '/signin';
+    return false;
+  } else {
+    return true;
+  }
 }
+
+if (token) {
+  if (checkTokenExp(token)) {
+    setAuthToken(token);
+    userId = decode(token)._id;
+  }
+}
+
+// let token = localStorage.getItem("token");
+// let userId = "";
+// if (token) {
+//   setAuthToken(token);
+//   userId = decode(token)._id;
+// }
 
 const initialState = {
   isAuthenticated: token,
