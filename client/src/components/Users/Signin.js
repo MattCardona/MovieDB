f4028e49fd729c3def15df9133c8f35f54652f18
Matt <boblikesbbq@gmail.com>
2020-03-22 12:34:32 -0700

    Signin handle errros

--- client/src/components/Users/Signin.js
+++ client/src/components/Users/Signin.js
@@ -7,24 +7,33 @@ import { signin } from '../../actions/userActions';
 class Signin extends Component {
   state = {
     username: "",
-    password: ""
+    password: "",
+    error: ""
   }
   onChange = (e) => {
     let prop = e.target.name;
     let val = e.target.value;
 
     this.setState(() => ({
-      [prop]: val
+      [prop]: val,
+      error: ""
     }))
   }
   onSubmit = (e) => {
     e.preventDefault();
     // console.log(this.state);
-    this.props.signin(this.state, () => {
-      this.props.history.push("/");
+    this.props.signin(this.state, (e) => {
+      if (e) {
+        this.setState(() => ({
+          error: e
+        }));
+      } else {
+        this.props.history.push("/");
+      }
     });
   }
   render() {
+    const e = this.state.error;
     return (
       <div
         className="container-fluid"
@@ -33,10 +42,12 @@ class Signin extends Component {
         <Navbar />
         <div style={{ paddingTop: "100px" }}>
           <h1>Sign in</h1>
+          {e.error ? <p className="error">{e.error}</p> : null}
           <form onSubmit={this.onSubmit}
             style={{ textAlign: "center" }}
           >
             <div className="form-group">
+              {e.username ? <p className="error">{e.username}</p> : null}
               <label htmlFor="username"></label>
               <input
                 id="username"
@@ -49,6 +60,7 @@ class Signin extends Component {
               />
             </div>
             <div className="form-group">
+              {e.password ? <p className="error">{e.password}</p> : null}
               <label htmlFor="password"></label>
               <input
                 style={{ textAlign: "center", width: "50%" }}
