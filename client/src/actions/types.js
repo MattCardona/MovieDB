38a76dc51227c94077094ecf9b8518b4faaf075b
Matt <boblikesbbq@gmail.com>
2020-03-29 11:51:49 -0700

    Create APPEND_NOW_PLAYING type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
 export const NOW_PLAYING = "NOW_PLAYING";
+export const APPEND_NOW_PLAYING = "APPEND_NOW_PLAYING";
 export const POPULAR = "POPULAR";
 export const TOP_RATED = "TOP_RATED";
 export const SEARCHED_MOVIE = "SEARCHED_MOVIE";
