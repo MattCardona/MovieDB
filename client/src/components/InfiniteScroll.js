c129bc2324b48be70c89debf4f0eb9e82e0f1631
Matt <boblikesbbq@gmail.com>
2020-04-03 11:18:36 -0700

    Create InfiniteScroll component

---
+++ client/src/components/InfiniteScroll.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { appendMovies } from "../actions/moviesActions";
+
+class InfiniteScroll extends Component {
+  state = {
+    count: 1,
+    path: ""
+  }
+  componentDidMount() {
+    if (this.props.movies.length > 19) {
+      let prevCount = this.props.movies.length / 20;
+      console.log(`prevcocunt ${prevCount}`);
+      this.setState(prevState => ({
+        count: prevCount
+      }));
+    }
+    this.setState(() => ({
+      path: this.props.kind
+    }));
+    if (this.state.count <= 10) {
+      window.addEventListener("scroll", this.handleScroll);
+    }
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.kind !== prevProps.kind) {
+      this.setState(() => ({
+        path: this.props.kind,
+        count: 1
+      }));
+    }
+  }
+  handleScroll = () => {
+    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      // Do awesome stuff like loading more content!
+      // console.log("redbull", this.props.movies);
+      if (this.props.movies.length < 100) {
+        this.more();
+      }
+    }
+  }
+  more = async () => {
+    if (this.props.movies.length <= 19) {
+      this.setState(prevState => ({
+        count: 1
+      }));
+    }
+    await this.setState(prevState => ({
+      count: prevState.count += 1
+    }));
+    // console.log("Appppendix");
+    this.props.appendMovies(this.state.path, this.state.count);
+  }
+  render() {
+    return (
+      <React.Fragment></React.Fragment>
+    )
+  }
+}
+
+
+export default connect(undefined, { appendMovies })(InfiniteScroll);
