5fd36156af3197fbf5b14f4243d33e4444199315
Matt <boblikesbbq@gmail.com>
2020-05-20 14:14:30 -0700

    Make iframe responsive

--- client/src/components/Trailer.js
+++ client/src/components/Trailer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Trailer = ({ trailerIds }) => {
   return (
-    <div className="container-fluid">
+    <div className="container">
       {trailerIds.length > 0 ?
-        <div style={{ textAlign: "center", paddingTop: "20px", paddingBottom: "20px" }}>
+        <div className="trailer" style={{ textAlign: "center", paddingTop: "20px", paddingBottom: "20px" }}>
           <h2>Trailers</h2>
-          <iframe width="640" height="315" src={`https://www.youtube.com/embed/${trailerIds[0]}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe className="iframe" src={`https://www.youtube.com/embed/${trailerIds[0]}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
         :
         null
