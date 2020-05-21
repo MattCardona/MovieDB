1f09cb303fefbe35f28da14275106f88ba1a12fc
Matt <boblikesbbq@gmail.com>
2020-05-21 16:14:51 -0700

    Style Trailer component

--- client/src/components/Trailer.js
+++ client/src/components/Trailer.js
@@ -4,8 +4,9 @@ const Trailer = ({ trailerIds }) => {
   return (
     <div className="container">
       {trailerIds.length > 0 ?
-        <div className="trailer" style={{ textAlign: "center", paddingTop: "20px", paddingBottom: "20px" }}>
-          <h2>Trailers</h2>
+        <div className="trailer">
+          <h2>Trailer</h2>
+          <hr />
           <iframe className="iframe" src={`https://www.youtube.com/embed/${trailerIds[0]}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
         :
