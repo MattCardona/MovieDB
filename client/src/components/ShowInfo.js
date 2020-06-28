bf7dd67739bab79a0c71d22f1b4d09e4c2768782
Matt <boblikesbbq@gmail.com>
2020-06-28 05:26:07 -0700

    ShowInfo showing recommended previous

--- client/src/components/ShowInfo.js
+++ client/src/components/ShowInfo.js
@@ -51,6 +51,8 @@ class ShowInfo extends Component {
         return <i onClick={this.handleClick} className="fas fa-user hover-effect"> Actor/Actress</i>;
       case "similar":
         return <i onClick={this.handleClick} className="fas fa-long-arrow-alt-left hover-effect point"> Previous</i>;
+      case "recommended":
+        return <i onClick={this.handleClick} className="fas fa-long-arrow-alt-left hover-effect point"> Previous</i>;
       default:
         return <i onClick={this.handleClick} className="fas fa-home hover-effect"> Home</i>;
 
