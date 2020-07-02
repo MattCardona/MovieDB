f27362962122ff6d7cff44aa93d1620b113836c6
Matt <boblikesbbq@gmail.com>
2020-07-02 15:36:43 -0700

    Add more styles to sort dropdown

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -110,13 +110,13 @@ class Home extends Component {
 
         <div className="container" id="movie-container">
 
-          <nav className="navbar  navbar-expand-lg navbar-dark">
+          <nav role="navigation" className="navbar  navbar-expand-lg navbar-dark">
             {/* <a class="navbar-brand" href="#"></a> */}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-burger" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="" id="navbar-burger">
+            <div className="collapse navbar-collapse" id="navbar-burger">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
                   <p ref={this.now_playing} className="nav-link hover-effect" id="now-playing" onClick={this.handleNowPlaying}>Now Playing<span className="sr-only">(current)</span></p>
