47262a335d8d4c45bc511cd93a9fd20e4643109f
Matt <boblikesbbq@gmail.com>
2020-07-24 09:09:48 -0700

    Add to /users route

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,21 @@ const requireAuth = passport.authenticate("jwt", { session: false });
 const Users = require("../models/Users");
 
 router.get("/", requireAuth, (req, res) => {
-  res.status(200).json({ "sucess": "working on the user routes" });
+  const { _id } = req.user;
+
+  Users.findById(_id)
+    .then(foundUser => {
+      if (!foundUser) {
+        // some type of error users was not found
+        res.status(400).json({ "error": "Something went wrong" });
+      }
+      const { username } = foundUser;
+      const user = { username };
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    })
 });
 
 module.exports = router;
