87368beeca30909d86e53321eacd2eb000c271c0
Matt <boblikesbbq@gmail.com>
2020-07-30 17:37:51 -0700

    Remove unnecessary stuff

--- client/src/components/Users/UsersProfile.js
+++ client/src/components/Users/UsersProfile.js
@@ -3,15 +3,9 @@ import Axios from 'axios';
 
 class UsersProfile extends Component {
   async componentDidMount() {
-    // if not authorized make them create an accont will make a Private HOC to wrap private routes but for now this will do
     try {
-      if (!Axios.defaults.headers.common["Authorization"]) {
-        this.props.history.push("/signup")
-      } else {
-        const { data } = await Axios.get("/users");
-        console.log("userData", data);
-
-      }
+      const { data } = await Axios.get("/users");
+      console.log("userData", data);
     } catch (error) {
       console.log(error.response);
     }
@@ -19,7 +13,7 @@ class UsersProfile extends Component {
   render() {
     return (
       <div>
-        This will be the Users Profile Need to make it a AUTH HOC so only if you are signed in and have an account you can view
+        UsersProfile Component
       </div>
     )
   }
