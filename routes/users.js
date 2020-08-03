483f756a80e1125bd1673a976e3feaca6446412f
Matt <boblikesbbq@gmail.com>
2020-08-03 14:45:30 -0700

    Send back Users saved movies

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,28 @@ const passportServices = require("../services/passport");
 const requireAuth = passport.authenticate("jwt", { session: false });
 
 const Users = require("../models/Users");
+const Movies = require("../models/Movies");
 
 router.get("/", requireAuth, (req, res) => {
   const { _id } = req.user;
 
   Users.findById(_id)
+    .populate("movies")
+    .exec()
     .then(foundUser => {
       if (!foundUser) {
         // some type of error users was not found
         res.status(400).json({ "error": "Something went wrong" });
       }
-      const { username } = foundUser;
-      const user = { username };
+      const { username, movies } = foundUser;
+      const user = { username, movies };
       res.status(200).json(user)
     })
     .catch(error => {
       res.status(400).json(error);
     })
 });
+
+
 
 module.exports = router;
