e8206e42f8381066c8ea86ca4b2b183d30f83920
Matt <boblikesbbq@gmail.com>
2020-08-04 17:54:48 -0700

    Consume user movies from backend api

--- client/src/components/Users/UsersProfile.js
+++ client/src/components/Users/UsersProfile.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import Axios from 'axios';
 
 class UsersProfile extends Component {
+  state = {
+    movies: []
+  }
   async componentDidMount() {
     try {
       const { data } = await Axios.get("/users");
       console.log("userData", data);
+      this.setState(() => ({
+        movies: [...data.movies]
+      }))
     } catch (error) {
       console.log(error.response);
     }
@@ -14,6 +20,15 @@ class UsersProfile extends Component {
     return (
       <div>
         UsersProfile Component
+
+        {this.state.movies.length ?
+          <div className="user-favMovies">
+            <h1>Favorite / Watch later list</h1>
+
+          </div>
+          :
+          null
+        }
       </div>
     )
   }
