936f512387a15e80abda4d9ca4705d6dd16036fe
Matt <boblikesbbq@gmail.com>
2020-10-03 15:40:09 -0700

    Create GET_USERS_SAVED type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -24,3 +24,4 @@ export const SIGNIN_USER = "SIGNIN_USER";
 export const SIGNOUT_USER = "SIGNOUT_USER";
 export const GET_USER_INFO = "GET_USER_INFO";
 export const SAVE_MOVIE = "SAVE_MOVIE";
+export const GET_USERS_SAVED = "GET_USERS_SAVED";
