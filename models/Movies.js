5a7ee38bcfe450addacf9cfde18d067ff8d46214
Matt <boblikesbbq@gmail.com>
2021-04-07 06:08:45 -0700

    Add a createdAt prop to Movies model

--- models/Movies.js
+++ models/Movies.js
@@ -4,7 +4,11 @@ const moviesSchema = new mongoose.Schema({
 	title: String,
 	posterPath: String,
 	backdropPath: String,
-	movieId: String
+	movieId: String,
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 const Movies = mongoose.model("Movies", moviesSchema);
